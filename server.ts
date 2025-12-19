9d277c9b01b225c94dc12b651be196c92e4cbd04
TheWeebSamurai ‎ <kuttykhalid198@gmail.com>
2025-12-19 11:19:41 +0530

    Transcript bug fix

--- server.ts
+++ server.ts
@@ -47,7 +47,7 @@ app.get(
         return res.status(400).send("Invalid transcript ID");
       }
   
-      const filePath = path.join(__dirname, "transcripts", `${id}`);
+      const filePath = path.join(__dirname, "transcripts", `${id}.html`);
   
       if (!fs.existsSync(filePath)) {
         return res
